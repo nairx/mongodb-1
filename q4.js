b1930ef0bd1548f7558a4b69bc779a5f27eea96f
nairx <nairxprv@gmail.com>
2026-02-02 13:39:04 +0530

    updated

--- q4.js
+++ q4.js
@@ -12,3 +12,15 @@ db.employees.find(
     {_id:0,name:1,salary:1}
 )
 
+db.employees.find().limit(3)
+
+db.employees.find().skip(1)
+
+db.employees.find().limit(3).skip(1)
+
+db.employees.find().sort({name:-1})
+
+db.employees.find().limit(3).skip(1).sort({name:-1})
+
+db.employees.find().count()
+
